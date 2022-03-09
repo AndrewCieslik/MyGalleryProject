3ac771db28141ff480f037f70e16a29ef9d238a2
Andrew Cieslik <cieslik.andrew@gmail.com>
2022-03-09 21:14:26 +0100

    add Rational.java

--- ObiektoweZad3/src/Rational.java
+++ ObiektoweZad3/src/Rational.java
@@ -1,4 +1,4 @@
-public class Rational implements Comparable<Rational>{          //+ comparable
+public class Rational implements Comparable<Rational> {          //+ comparable
     private int numerator;
     private int denominator;
 
@@ -23,12 +23,12 @@ public class Rational implements Comparable<Rational>{          //+ comparable
         this.denominator = denominator;
     }
 
-    public Rational add(Rational arg) {      // +
-        return new Rational(this.numerator * arg.denominator + arg.numerator * this.denominator, this.denominator * arg.denominator);
+    public Rational add(Rational arg) {     // +
+        return new Rational((this.numerator * arg.denominator + arg.numerator * this.denominator), this.denominator * arg.denominator);
     }
 
     public Rational mul(Rational arg) {     // *
-        return new Rational(this.numerator * arg.numerator,this.denominator * arg.denominator);
+        return new Rational(this.numerator * arg.numerator, this.denominator * arg.denominator);
     }
 
     public Rational sub(Rational arg) {      // -
@@ -36,23 +36,35 @@ public class Rational implements Comparable<Rational>{          //+ comparable
     }
 
     public Rational div(Rational arg) {     // /
-        return new Rational(this.numerator * arg.denominator,this.denominator * arg.numerator);
+        return new Rational(this.numerator * arg.denominator, this.denominator * arg.numerator);
     }
 
     public boolean equals(Rational arg) {   // ==
         return this.numerator * arg.denominator == this.denominator * arg.numerator;
     }
 
-   public int compareTo(Rational arg){         //comparable (returning int: 0, -1 or 1)
-       if(this.numerator * arg.denominator == this.denominator * arg.numerator)
-           return 0;
-       else if(this.numerator * arg.denominator > arg.numerator * this.denominator)
-           return 1;
-       else
-           return -1;
-   }
+    public int compareTo(Rational arg) {         //comparable (returning int: 0, -1 or 1)
+        if (this.numerator * arg.denominator == this.denominator * arg.numerator)
+            return 0;
+        else if (this.numerator * arg.denominator > arg.numerator * this.denominator)
+            return 1;
+        else
+            return -1;
+    }
 
-   public String toString() {
-        return this.numerator + "/" + this.denominator;
-   }
+    public String toString() {
+        return this.numerator/gcd(this.numerator,this.denominator) + "/" + this.denominator/gcd(this.numerator,this.denominator);
+                        //   /gcd() added to shorten result
+    }
+
+    public static int gcd(int a, int b)  //greatest common divisor
+    {
+        if (a == 0) {
+            return b;
+        } else {
+            return gcd(b % a, a);
+        }
+    }
 }
+
+
