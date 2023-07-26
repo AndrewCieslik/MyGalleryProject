ba830c957bd1d16b0460665e00de83c40774ad6a
cieslik.andrew <cieslik.andrew@gmail.com>
2023-07-26 11:19:34 +0200

    add thymeleaf

--- MyShop/src/main/java/com/nopainnogame/myShop/controller/HomeController.java
+++ MyShop/src/main/java/com/nopainnogame/myShop/controller/HomeController.java
@@ -2,15 +2,17 @@ package com.nopainnogame.myShop.controller;
 
 import com.nopainnogame.myShop.model.Item;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.math.BigDecimal;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public Item home(){
-        return new Item("Pen", new BigDecimal("20.0"), "https://images.pexels.com/photos/261450/pexels-photo-261450.jpeg?cs=srgb&dl=pexels-pixabay-261450.jpg&fm=jpg");
+    public String home(Model model){
+        model.addAttribute("item",new Item("Pen", new BigDecimal("20.0"), "https://images.pexels.com/photos/261450/pexels-photo-261450.jpeg?cs=srgb&dl=pexels-pixabay-261450.jpg&fm=jpg"));
+        //return );
+        return "home";
     }
 }
