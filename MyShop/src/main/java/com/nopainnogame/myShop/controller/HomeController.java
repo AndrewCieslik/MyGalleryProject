54210b2d3c6e0d793885cc71c5200eeb1600f412
cieslik.andrew <cieslik.andrew@gmail.com>
2023-07-26 10:57:18 +0200

    return Item

--- MyShop/src/main/java/com/nopainnogame/myShop/controller/HomeController.java
+++ MyShop/src/main/java/com/nopainnogame/myShop/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.nopainnogame.myShop.controller;
 
+import com.nopainnogame.myShop.model.Item;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.math.BigDecimal;
+
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home(){
-        return "home";
+    public Item home(){
+        return new Item("Pen", new BigDecimal("20.0"), "https://images.pexels.com/photos/261450/pexels-photo-261450.jpeg?cs=srgb&dl=pexels-pixabay-261450.jpg&fm=jpg");
     }
 }
