e4307a167bd92728bf01e6bb79e2a0e6d372c402
Andrew <101130154+AndrewCieslik@users.noreply.github.com>
2023-07-27 22:59:10 +0200

    Delete Egzamin Java Rok1 directory

--- Egzamin Java Rok1/EgzaminPlikTekstowy/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        EgzaminPlikTekstowy emerytury = new EgzaminPlikTekstowy();
-        emerytury.przepiszPlik("nazwiska");
-    }
-}
