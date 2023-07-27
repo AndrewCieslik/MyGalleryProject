de11e33538a581f58aaad9832e6c7dfd1da1ff35
cieslik.andrew <cieslik.andrew@gmail.com>
2023-07-27 21:21:14 +0200

    ItemRepository

--- MyShop/src/main/java/com/nopainnogame/myShop/controller/AdminController.java
+++ MyShop/src/main/java/com/nopainnogame/myShop/controller/AdminController.java
@@ -1,6 +1,8 @@
 package com.nopainnogame.myShop.controller;
 
 import com.nopainnogame.myShop.model.Item;
+import com.nopainnogame.myShop.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public AdminController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
     @GetMapping
     private String adminPage() {
         return "adminview/addItem";
@@ -16,7 +25,7 @@ public class AdminController {
 
     @PostMapping
     private String addItem(Item item) {
-        HomeController.items.add(item);
+        itemRepository.save(item);
         return "redirect:/";
     }
 }
--- MyShop/src/main/java/com/nopainnogame/myShop/controller/HomeController.java
+++ MyShop/src/main/java/com/nopainnogame/myShop/controller/HomeController.java
@@ -1,27 +1,29 @@
 package com.nopainnogame.myShop.controller;
 
 import com.nopainnogame.myShop.model.Item;
+import com.nopainnogame.myShop.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HomeController {
-    static List<Item> items = new ArrayList<>();
+    private final ItemRepository itemRepository;
 
-    static {
-        items.add(new Item("Pen", new BigDecimal("20.0"), "https://images.pexels.com/photos/261450/pexels-photo-261450.jpeg?cs=srgb&dl=pexels-pixabay-261450.jpg&fm=jpg"));
-        items.add(new Item("Benyar Blue", new BigDecimal("25.23"), "https://tmzegarki.pl/wp-content/uploads/2023/02/3688043e4e0fcb1830b744ac7c1a2f46.jpg"));
-        items.add(new Item("Benyar X", new BigDecimal("30.23"), "https://tmzegarki.pl/wp-content/uploads/2020/01/cz-cz-1-miniatura.jpg"));
+    @Autowired
+    public HomeController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
     }
 
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("items", items);
+    public String home(Model model, HttpSession httpSession) {
+        model.addAttribute("items", itemRepository.findAll());
         return "home";
     }
 }
