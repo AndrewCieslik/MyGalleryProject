63516102f2eb46b71d730da10c1894d53a276e70
cieslik.andrew <cieslik.andrew@gmail.com>
2023-08-11 21:45:23 +0200

    oItem

--- MyShop/src/main/java/com/nopainnogame/myShop/controller/HomeController.java
+++ MyShop/src/main/java/com/nopainnogame/myShop/controller/HomeController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpSession;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -30,7 +31,12 @@ public class HomeController {
 
     @GetMapping("/add/{itemId}")
     public String addItemToCart(@PathVariable("itemId") Long itemId, Model model, HttpSession session) {
-        @SuppressWarnings("unchecked") List<Item> cart = (List<Item>) session.getAttribute("cart");
+        @SuppressWarnings("unchecked")
+        List<Item> cart = (List<Item>) session.getAttribute("cart");
+        if(cart == null) {
+            cart = new ArrayList<>();
+        }
+        Optional<Item> oItem = itemRepository.findById(itemId);
         model.addAttribute("items", itemRepository.findAll());
         return "home";
     }
