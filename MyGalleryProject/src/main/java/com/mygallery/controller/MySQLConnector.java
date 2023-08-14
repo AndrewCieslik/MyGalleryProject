5076c538f4e2e7dd6583fe12a0dec7362f9c56d1
cieslik.andrew <cieslik.andrew@gmail.com>
2023-08-14 21:47:18 +0200

    findAvailableId()

--- MyGalleryProject/src/main/java/com/mygallery/controller/MySQLConnector.java
+++ MyGalleryProject/src/main/java/com/mygallery/controller/MySQLConnector.java
@@ -2,6 +2,7 @@ package com.mygallery.controller;
 
 import lombok.Getter;
 import lombok.Setter;
+
 import java.sql.*;
 
 @Getter
@@ -43,4 +44,50 @@ public class MySQLConnector {
             e.printStackTrace();
         }
     }
+
+    public int findAvailableId() {
+        int availableId = 1;
+
+        boolean found = false;
+
+        do {
+            ResultSet rs = null;
+            PreparedStatement pstmt = null;
+
+            try {
+                String query = "SELECT user_id FROM users WHERE user_id = ? OR user_id = ?";
+                pstmt = conn.prepareStatement(query);
+                pstmt.setInt(1, availableId);
+                pstmt.setInt(2, availableId + 1);
+
+                rs = pstmt.executeQuery();
+
+                int firstId = 0;
+                int secondId = 0;
+
+                while (rs.next()) {
+                    int userId = rs.getInt("user_id");
+                    if (userId == availableId) {
+                        firstId = userId;
+                    } else if (userId == availableId + 1) {
+                        secondId = userId;
+                    }
+                    if (secondId == firstId + 1) {
+                        availableId = secondId + 1;
+                    }
+                }
+
+                if (secondId != firstId + 1) {
+                    found = true;
+                }
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } while (!found);
+
+        return availableId;
+    }
+
+
 }
